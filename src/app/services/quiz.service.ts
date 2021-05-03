d81759ae47cab73e032648c6497dc791dea5b564
unknown <vivekkumar5708@gmail.com>
2021-05-03 18:32:26 +0530

    angular quiz app for simplilearn assesment

---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class QuizService {
+
+  constructor(private http: HttpClient) { }
+
+  get(url: string) {
+    return this.http.get(url);
+  }
+
+  getAll() {
+    return [
+      { id: 'data/javascript.json', name: 'JavaScript' }
+    ];
+  }
+
+}
